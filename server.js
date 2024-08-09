bf4d3ca3493239027488eaeb986d8d4a3c05a19e
Kabir Khithani <kabirkhithani@gmail.com>
2024-08-09 11:32:57 +0400

    commit-2

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+//require('dotenv').config(); // Load environment variables
 
-//routes
-app.get('/', (req,res) => {
-    res.send("Hello Node")
-})
-app.get('/blog', (req, rest) => {
-    res.send("Hello blog, this is Kabir")
-})
+// Routes
+app.get('/', (req, res) => {
+    res.send("Hello Node");
+});
 
-mongoose.set("strictQuery", false)
+app.get('/blog', (req, res) => {
+    res.send("Hello blog, this is Kabir");
+});
+
+mongoose.set("strictQuery", false);
+
+const mongoDbName = "nodejs=project-demo"; // Your database name
+const mongoURI = `mongodb://localhost:27017/${mongoDbName}`; // Local MongoDB URI
+
 mongoose
-.connect('mongodb+srv://admin:idcnodejsprojectdemo@nodejsprojectdemo.oef27.mongodb.net/NodeJS-Project-Demo?retryWrites=true&w=majority&appName=NodeJS-Project-Demo')
-.then(() => {
-    console.log("Connected to MongoDB")
-    app.listen(3000, ()=> {
-        console.log('Node app is running on port 3000')
+    .connect(mongoURI) // Use the local MongoDB URI
+    .then(() => {
+        console.log("Connected to MongoDB");
+        app.listen(3000, () => {
+            console.log("Node app is running on port 3000");
+        });
     })
-}).catch((error) => {
-    console.log(error)
-})
+    .catch((error) => {
+        console.log(error);
+    });
