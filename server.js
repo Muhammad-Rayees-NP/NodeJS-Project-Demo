0a6473aa75a4cb6345a4e023a0cab2e11f0b766b
Kabir Khithani <kabirkhithani@gmail.com>
2024-08-13 10:26:22 +0400

    CRUD processes complete

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const product = require('.models/productModel')
 const app = express();
-//require('dotenv').config(); // Load environment variables
 
-
-// new changes
-
+app.use(express.json);
+app.use(express.urlencoded({extended: false}))
 // Routes
 app.get('/', (req, res) => {
     res.send("Hello Node");
@@ -15,13 +14,68 @@ app.get('/blog', (req, res) => {
     res.send("Hello blog, this is Kabir");
 });
 
-mongoose.set("strictQuery", false);
+app.get('/products', async(req, res)=> {
+    try {
+        const {id} = req.params;
+        const products = await Product.findById(id);
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
 
-const mongoDbName = "nodejs=project-demo"; // Your database name
-const mongoURI = `mongodb://localhost:27017/${mongoDbName}`; // Local MongoDB URI
+app.get('/products/:id', async(req, res)=> {
+    try {
+        const product = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.post('/product', async(req, res) => {
+    try {
+        const product = await Product/create(req.body)
+        res.status(200).json(product);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: error.message});
+    }
+})
+
+app.put('products: id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        if (!product) {
+            return res.status(404).json({message: 'cannot find any productwith ID ${id}'}) 
+        }
+        const updatedProduct= await Product.findByID(id);
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
+app.delete('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByAndDelete(id);
+        if(!product) {
+            return res.status(404).json({message: 'cannot find any productwith ID ${id}'}) 
+        }
+        ResizeObserverSize.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message});        
+    }
+})
+
+mongoose.set("strictQuery", false);
+const mongoDbName = "nodejs=project-demo"; 
+const mongoURI = `mongodb://localhost:27017/${mongoDbName}`;
 
 mongoose
-    .connect(mongoURI) // Use the local MongoDB URI
+    .connect(mongoURI)
     .then(() => {
         console.log("Connected to MongoDB");
         app.listen(3000, () => {
